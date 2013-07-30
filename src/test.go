febc6f63a7ac4ae45ebbc26c0226da724a870729
Guillaume ALAUX <guillaume@archlinux.org>
2013-07-31 00:07:04 +0200

    Finished quicksort

--- src/test.go
+++ src/test.go
@@ -2,15 +2,25 @@ package main
 
 import (
   "fmt"
+//  "os"
 )
 
-func getPivotIdx(list []int) int {
+var Max_depth = 10
+var Depth     =  0
+
+func getPivotIdx(list []int, start int, end int) int {
   return 0
 }
 
-func partition(list []int) []int {
-  i := 1
-  for j := 1; j < len(list); j++ {
+// start: index of list to start partitionning (included)
+// end  : index of list to end partitionning   (excluded)
+func partition(list []int, start int, end int) ([]int, int) {
+  if end - start == 1 {
+    return list, start
+  }
+
+  i := start
+  for j := 1; j < end; j++ {
     if list[j] < list[0] {
       // swap i and j
       tmp := list[i]
@@ -21,23 +31,39 @@ func partition(list []int) []int {
     }
   }
 
+  newPivotIdx := i - 1
   tmp := list[0]
-  list[0] = list[i - 1]
-  list[i - 1] = tmp
+  list[0] = list[newPivotIdx]
+  list[newPivotIdx] = tmp
 
-  return list
+  return list, newPivotIdx
 }
 
-func quicksort(list []int) []int {
-  pivotIdx := getPivotIdx(list)
+// start: index of list on which to start quicksort (included)
+// end  : index of list on which to end quicksort   (included)
+func quicksort(list []int, start int, end int) []int {
+/*
+  Depth++
+  if Depth > Max_depth {
+    os.Exit(1)
+  }
+  */
+  fmt.Println("quicksort", list, ",", start, ",", end)
+  pivotIdx := getPivotIdx(list, start, end)
   fmt.Printf("Pivot: list[%v] = %v\n", pivotIdx, list[pivotIdx])
 
   // Swap pivot for first position
-  tmp := list[0]
-  list[0] = list[pivotIdx]
+  tmp := list[start]
+  list[start] = list[pivotIdx]
   list[pivotIdx] = tmp
 
-  partList := partition(list)
+  partList, newPivotIdx := partition(list, 1, len(list))
+  if start < newPivotIdx {
+    quicksort(list, start, newPivotIdx - 1)
+  }
+  if newPivotIdx < end {
+    quicksort(list, newPivotIdx + 1, end)
+  }
 
   return partList
 }
@@ -45,6 +71,6 @@ func quicksort(list []int) []int {
 func main() {
   fmt.Println("Starting")
   list := []int {4, 3, 2, 6, 1, 5}
-  fmt.Println(quicksort(list))
+  fmt.Println(quicksort(list, 0, len(list) - 1))
   fmt.Println("Done")
 }
