19f7b539652094fbc2352e8e9b1f0f36193f5f10
Guillaume ALAUX <guillaume@archlinux.org>
2013-08-02 20:40:38 +0200

    First version of quisksort OK (dumb pivot chosen)

--- src/test.go
+++ src/test.go
@@ -17,31 +17,29 @@ var Depth     =  0
 //}
 
 func getPivotIdx(list *[]int, start int, end int) int {
-  return 0
+  return start
 }
 
 // start: index of list to start partitionning (included)
 // end  : index of list to end partitionning   (included)
 func partition(list *[]int, start int, end int) int {
-  //if end - start == 1 {
-  //  return list, start
-  //}
+  i := start + 1
+  if end != start {
+    for j := start + 1; j <= end; j++ {
+      if (*list)[j] < (*list)[start] {
+        // swap i and j
+        tmp       := (*list)[i]
+        (*list)[i] = (*list)[j]
+        (*list)[j] = tmp
 
-  i := start
-  for j := 1; j <= end; j++ {
-    if (*list)[j] < (*list)[0] {
-      // swap i and j
-      tmp := (*list)[i]
-      (*list)[i] = (*list)[j]
-      (*list)[j] = tmp
-
-      i++
+        i++
+      }
     }
   }
 
   newPivotIdx := i - 1
-  tmp := (*list)[0]
-  (*list)[0] = (*list)[newPivotIdx]
+  tmp := (*list)[start]
+  (*list)[start] = (*list)[newPivotIdx]
   (*list)[newPivotIdx] = tmp
 
   return newPivotIdx
@@ -50,19 +48,11 @@ func partition(list *[]int, start int, end int) int {
 // start: index of list on which to start quicksort (included)
 // end  : index of list on which to end quicksort   (included)
 func quicksort(list *[]int, start int, end int) {
-/*
-  Depth++
-  if Depth > Max_depth {
-    os.Exit(1)
-  }
-  */
-  //fmt.Println("quicksort", list, ",", start, ",", end)
-  if start == end {
+  if start >= end {
     return
   }
 
   pivotIdx := getPivotIdx(list, start, end)
-  //fmt.Printf("Pivot: list[%v] = %v\n", pivotIdx, (*list)[pivotIdx])
 
   // Swap pivot for first position
   tmp := (*list)[start]
@@ -70,12 +60,8 @@ func quicksort(list *[]int, start int, end int) {
   (*list)[pivotIdx] = tmp
 
   newPivotIdx := partition(list, start, end)
-  //if start < newPivotIdx {
-    quicksort(list, start, newPivotIdx - 1)
-  //}
-  if newPivotIdx < end {
-    quicksort(list, newPivotIdx + 1, end)
-  }
+  quicksort(list, start, newPivotIdx - 1)
+  quicksort(list, newPivotIdx + 1, end)
 }
 
 func readFile(fname string) (nums []int, err error) {
@@ -99,17 +85,19 @@ func readFile(fname string) (nums []int, err error) {
 
 func main() {
   fmt.Println("Starting")
-  //list, err := readFile("./QuickSort_light.txt")
-  //list, err := readFile("./QuickSort.txt")
-  //if err != nil { panic(err) }
-  list := []int{3, 5, 4, 1, 6, 7, 2}
+
+  list, err := readFile("./QuickSort.txt")
+  if err != nil { panic(err) }
+  //list := []int{3, 5, 4, 1, 6, 7, 2}
   fmt.Println("Found", len(list), "integers")
 
   t0 := time.Now()
   quicksort(&list, 0, len(list) - 1)
   t1 := time.Now()
   fmt.Println("Done. FYI, the resulting array has size", len(list))
-  //fmt.Println(res)
+  fmt.Println(list)
   fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
+
+
   fmt.Println("Done")
 }
